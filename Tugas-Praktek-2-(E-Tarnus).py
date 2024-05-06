62e8458191dad8ef0465d7520c9336fca4b1a492
Reza <97615448+Code-With-Reza@users.noreply.github.com>
2024-05-06 02:44:18 +0000

    a

--- Tugas-Praktek-2-(E-Tarnus).py
+++ Tugas-Praktek-2-(E-Tarnus).py
@@ -1,7 +1,7 @@
 def Nomor_1():
     angka = [10, 5, 7, 3, 15, 20]
-    print("Angka terkecil:", min(angka))
-    print("Angka terbesar:", max(angka))
+    print("Angka terkecil :", min(angka))
+    print("Angka terbesar :", max(angka))
 
 def Nomor_2():
     import random
@@ -9,7 +9,7 @@ def Nomor_2():
     angka_random = random.randint(1, 100)
     tebakan = None
     while tebakan != angka_random:
-        tebakan = int(input("Tebak angka (antara 1 dan 100): "))
+        tebakan = int(input("Tebak angka (antara 1 dan 100) : "))
         if tebakan < angka_random:
             print("Tebakan terlalu kecil.")
         elif tebakan > angka_random:
@@ -26,5 +26,5 @@ def Nomor_3():
         for x in bil:
             hasil = hasil * x
         
-        print(str(inputs)+"! = ",hasil)
+        print(str(inputs)+"! =",hasil)
 
