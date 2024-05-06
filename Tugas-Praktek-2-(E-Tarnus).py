0e22ad66613cbd3471a095647d20b3f23fba5917
Reza <97615448+Code-With-Reza@users.noreply.github.com>
2024-05-06 02:26:08 +0000

    updated

--- Tugas-Praktek-2-(E-Tarnus).py
+++ Tugas-Praktek-2-(E-Tarnus).py
@@ -18,12 +18,12 @@ print("Selamat! Anda berhasil menebak angka", angka_random)
 sleep(2)
 
 # Nomor 3
-def hitung_faktorial(n):
+def faktorial(n):
     if n == 0:
         return 1
     else:
-        return n * hitung_faktorial(n - 1)
+        return n * faktorial(n - 1)
 
 for i in range(1, 101):
-    print("Faktorial dari", i, "adalah", hitung_faktorial(i))
+    print("Faktorial dari", i, "adalah", faktorial(i))
 
