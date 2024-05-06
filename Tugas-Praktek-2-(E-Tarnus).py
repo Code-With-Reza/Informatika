4cba6ff723cb618ebb3f3c39c898e5c8221b1a38
Reza <97615448+Code-With-Reza@users.noreply.github.com>
2024-05-06 02:36:17 +0000

    aaa

--- Tugas-Praktek-2-(E-Tarnus).py
+++ Tugas-Praktek-2-(E-Tarnus).py
@@ -1,29 +1,30 @@
-# Nomor 1
-angka = [10, 5, 7, 3, 15, 20]
-print("Angka terkecil:", min(angka))
-print("Angka terbesar:", max(angka))
+def Nomor_1():
+    angka = [10, 5, 7, 3, 15, 20]
+    print("Angka terkecil:", min(angka))
+    print("Angka terbesar:", max(angka))
 
-# Nomor 2
-import random
+def Nomor_2():
+    import random
+    
+    angka_random = random.randint(1, 100)
+    tebakan = None
+    while tebakan != angka_random:
+        tebakan = int(input("Tebak angka (antara 1 dan 100): "))
+        if tebakan < angka_random:
+            print("Tebakan terlalu kecil.")
+        elif tebakan > angka_random:
+            print("Tebakan terlalu besar.")
+    print("Selamat! Anda berhasil menebak angka", angka_random)
+    sleep(2)
 
-angka_random = random.randint(1, 100)
-tebakan = None
-while tebakan != angka_random:
-    tebakan = int(input("Tebak angka (antara 1 dan 100): "))
-    if tebakan < angka_random:
-        print("Tebakan terlalu kecil.")
-    elif tebakan > angka_random:
-        print("Tebakan terlalu besar.")
-print("Selamat! Anda berhasil menebak angka", angka_random)
-sleep(2)
-
-# Nomor 3
-def faktorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * faktorial(n - 1)
-
-for i in range(1, 101):
-    print("Faktorial dari", i, "adalah", faktorial(i))
+def Nomor_3():
+    for banyak in range(1,101):
+        inputs = int(banyak)
+        bil = range(1,inputs+1)
+        hasil = 1
+        
+        for x in bil:
+            hasil = hasil * x
+        
+        print(str(inputs)+"! = ",hasil)
 
