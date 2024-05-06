3171dfa18af67a6d70a186682e36db985c51d2b7
Reza <97615448+Code-With-Reza@users.noreply.github.com>
2024-05-06 02:18:34 +0000

    done

---
+++ Tugas Praktek 2.py
@@ -0,0 +1,28 @@
+# Nomor 1
+angka = [10, 5, 7, 3, 15, 20]
+print("Angka terkecil:", min(angka))
+print("Angka terbesar:", max(angka))
+
+# Nomor 2
+import random
+
+angka_random = random.randint(1, 100)
+tebakan = None
+while tebakan != angka_random:
+    tebakan = int(input("Tebak angka (antara 1 dan 100): "))
+    if tebakan < angka_random:
+        print("Tebakan terlalu kecil.")
+    elif tebakan > angka_random:
+        print("Tebakan terlalu besar.")
+print("Selamat! Anda berhasil menebak angka", angka_random)
+sleep(2)
+
+# Nomor 3
+def hitung_faktorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * hitung_faktorial(n - 1)
+
+for i in range(1, 101):
+    print("Faktorial dari", i, "adalah", hitung_faktorial(i))
