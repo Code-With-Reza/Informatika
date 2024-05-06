1ec1e400b5608262c8b5f3675eafac355ebbd28b
Reza <97615448+Code-With-Reza@users.noreply.github.com>
2024-05-06 08:56:53 +0700

    update 3

---
+++ Operator.py
@@ -0,0 +1,29 @@
+a = 28
+b = 5
+
+#pertama
+print("a + b = ",a+b)
+print("a - b = ",a-b)
+print("a * b = ",a*b)
+print("a / b = ",a/b)
+print("a % b = ",a%b)
+print("a ** b = ",a**b)
+print("a // b = ",a//b)
+
+#kedua
+print("a == b : ",a==b)
+print("a != b : ",a!=b)
+print("a > b : ",a>b)
+print("a >= b : ",a>=b)
+print("a < b : ",a<b)
+print("a <= b : ",a<=b)
+
+#ketiga
+a=b
+print("a = b : a = ",a)
+a+=b
+print("a += b : a = ",a)
+a*=b
+print("a *= b : a = ",a)
+a**=b
+print("a **= b : a = ",a)
