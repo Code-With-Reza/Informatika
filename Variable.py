5d1efb5187cfd60e5daaa394ca2467a82f54bd95
Reza <97615448+Code-With-Reza@users.noreply.github.com>
2024-05-06 08:42:44 +0700

    update 2

---
+++ Variable.py
@@ -0,0 +1,21 @@
+#string
+divider = " "
+buah = "apel"
+
+#integer
+jumlah = 20
+orang = 6
+
+#float
+dapet = jumlah/orang
+
+#list
+nama = ["Raja","Acit","Zufar","Yafi"]
+
+#range
+jumlah_orang = range(4-1)
+
+
+sisa = jumlah-dimakan
+
+print(str(sisa) + buah)
